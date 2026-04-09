604c4bf0f4fe9fd476894898db71e1316a519d5e
arun <arungk808@gmail.com>
2026-04-10 02:55:37 +0530

    updated message again

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,6 +14,6 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Earth 🚀";
+        return "Hello from Earth. Its a beautiful day 🚀";
     }
 }
