fcdc28c60ef0830dcca312f73b350493590f9a08
arun <arungk808@gmail.com>
2026-05-20 17:00:39 +0530

    updated docker file again3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,6 +14,6 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Earth. Its a beautiful day 🚀";
+git         return "Hello from Earth. Its a beautiful day 🚀";
     }
 }
